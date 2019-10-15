a677f78b73d095151e3ec2f9a4c9c1c0fe047ba2
Manon18 <maryland16z18@gmail.com>
2019-10-15 17:50:42 +0300

    added carousel and changhed styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 import { ProductsListComponent } from './modules/products-list/products-list.component';
 import { BasketComponent } from './modules/basket/basket.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent},
   { path: 'products', component: ProductsListComponent },
   { path: 'basket', component: BasketComponent }
 ];
 
 @NgModule({
-  declarations: [ProductsListComponent, BasketComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
-  exports: [RouterModule, ProductsListComponent, BasketComponent],
-  entryComponents: [ProductsListComponent, BasketComponent]
+  declarations: [
+    HomeComponent, 
+    ProductsListComponent, 
+    BasketComponent
+  ],
+  imports: [BrowserModule, MDBBootstrapModule.forRoot(), RouterModule.forRoot(routes)],
+  exports: [
+    RouterModule, 
+    HomeComponent, 
+    ProductsListComponent, 
+    BasketComponent
+  ],
+  entryComponents: [HomeComponent, ProductsListComponent, BasketComponent]
 })
 export class AppRoutingModule { }
